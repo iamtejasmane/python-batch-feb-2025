5b398042bef64491501b9d1a235f85e7195af884
Tejas Mane <tejasmane485@gmail.com>
2025-03-04 17:28:26 +0530

    control flow uncomment

--- day4/control-flow.py
+++ day4/control-flow.py
@@ -4,54 +4,54 @@
 # Checks a condition and executes the indented block if the condition is True
 
 # Example: Checking voting eligibility
-age = 15
+# age = 15
 
-if age >= 18:
-    print("You are eligible to vote")
+# if age >= 18:
+#     print("You are eligible to vote")
 
 # 2. if-else Statement
 # Executes one block if the condition is True, otherwise executes the else block
 
-age = 25
+# age = 12
 
-if age >= 18:
-    print("You are eligible to vote")
-else:
-    print("You are not eligible to vote yet.")
+# if age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote yet.")
 
 # 3. if-elif-else Statement
 # Allows multiple conditions to be checked sequentially
 
-marks = 95
+# marks = 54
 
-# Grading System:
-# 90 and above      : Grade A
-# 75 to 89         : Grade B
-# 50 to 74         : Grade C
-# Below 50         : Grade F
+# # Grading System:
+# # 90 and above      : Grade A
+# # 75 to 89         : Grade B
+# # 50 to 74         : Grade C
+# # Below 50         : Grade F
 
-if marks > 100 or marks < 0:
-    print("Hey, please add proper numbers!")
-elif marks >= 90:
-    print("Congratulations! Your Grade is A!")
-elif marks >= 75:
-    print("Wow! Your grade is B")
-elif marks >= 50:
-    print("Work hard, your grade is C")
-else:
-    print("Sorry! You have failed!!")
+# if marks > 100 or marks < 0:
+#     print("Hey, please add proper numbers!")
+# elif marks >= 90:
+#     print("Congratulations! Your Grade is A!")
+# elif marks >= 75:
+#     print("Wow! Your grade is B")
+# elif marks >= 50:
+#     print("Work hard, your grade is C")
+# else:
+#     print("Sorry! You have failed!!")
 
 # Basic arithmetic operations
-print("Division:", 10 / 2)  # Division
-print("Remainder:", 10 % 2)  # Modulus (Remainder)
+# print("Division:", 10 / 2)  # Division
+# print("Remainder:", 10 % 2)  # Modulus (Remainder)
 
-# Checking if a number is even or odd
-num = int(input("Enter a number: "))
+# # Checking if a number is even or odd
+# num = int(input("Enter a number: "))
 
-if num % 2 == 0:
-    print("Even number")
-else:
-    print("Odd number")
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
 
 # User authentication system
 username = input("Enter your username: ")
